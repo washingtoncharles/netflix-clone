c2d2ab733fb91c097e216a0d5d4b8b38054a8524
Washington Charles <toncharles@live.com>
2023-10-22 11:57:03 -0300

    component row ajusted
    adjusted component row to display trailer on click

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,9 +1,30 @@
 import React, { useEffect } from "react";
 import { getMovies } from "../services/api";
 import { Container } from "./Row.style";
+import ReactPlayer from "react-player";
+import movieTrailer from "movie-trailer";
 
 export default function Row({title, path, isLarge}) {
   const [movies, setMovies] = React.useState([]);
+  const [trailerUrl, setTrailerUrl] = React.useState("");
+
+  //PEGAR URL DO TRAILER COM ACAO DE CLICK
+  const handleOnClick = (movie) => {
+    if (trailerUrl) { //CONDIÇÃO PARA FECHAR O VIDEO
+      setTrailerUrl("");
+    } else { //CONDIÇÃO PARA ABRE O VIDEO
+      movieTrailer(movie.title || movie.name || movie.original_name || "")
+        .then((url) => {
+          setTrailerUrl(url);
+          //setTrailerUrl("https://www.youtube.com/watch?v=ZnZqB5Z75zI"); 
+
+        })
+        .catch((error) => {
+          console.log("Error fetching movie trailer: ", error);
+        });
+    }
+  }
+
   const fetchMovies = async (_path) => {
     try {
       const data = await getMovies(_path);
@@ -27,6 +48,9 @@ export default function Row({title, path, isLarge}) {
           return (
             <img 
               className={isLarge ? "movie-card-large" : "movie-card"}
+              //FUNCTION OPEN VIDEO
+              onClick={() => handleOnClick(movie)}
+
               key={movie.id}
               src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} 
               alt={movie.name} 
@@ -34,6 +58,7 @@ export default function Row({title, path, isLarge}) {
           )
         })}
       </div>
+      {trailerUrl && <ReactPlayer playing={true} url={trailerUrl} />}
     </Container>
   )  
 }
